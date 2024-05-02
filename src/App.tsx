eb1d0d7ce7eefa098507a75efd9940eb2f11a8d0
Paulo-Pacheco-Junior <paulojrgyn88@gmail.com>
2024-05-02 15:34:44 -0300

    Todas as funcionalidades implementadas com Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { ChangeEvent, FormEvent, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import { Header } from './components/Header'
 import { TaskStatusBar } from './components/TaskStatusBar';
 import { ZeroTasks } from './components/ZeroTasks';
@@ -6,27 +8,74 @@ import { PlusCircle } from "@phosphor-icons/react";
 import styles from './App.module.css'
 import './global.css'
 
+interface TaskProps {
+  id: string;
+  text: string;
+  checked: boolean;
+}
+
 export function App() {
-  
-  const tasks = []; 
-  // const tasks = ['exibir TaskList'];
+  const [tasks, setTasks] = useState<TaskProps[]>([]);
+  const [task, setTask] = useState('');
+
+  function handleInputCreateTask(event: ChangeEvent<HTMLInputElement>) {
+    setTask(event.target.value)
+  }
+
+  function handleSubmitTask(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    if(task.length > 0 && task.trim() !== '') {
+      const newTask = {
+        id: uuidv4(),
+        text: task, 
+        checked: false
+      } 
+      setTasks([...tasks, newTask])
+      setTask('');
+    }
+  }
+
+  function onToggleTask(taskId: string) {
+    setTasks(tasks.map(task => {
+      if(task.id === taskId) {
+        return {...task, checked: !task.checked};
+      }
+      return task;
+    }));
+  }
+
+  function onDeleteTask(taskId: string) { 
+    setTasks(tasks.filter(task => task.id !== taskId));
+  }
 
   return (
     <div className={styles.wrapper}>
       <Header />
       <section className={styles.mainContent}>
-        <form className={styles.formTasks}>
+        <form onSubmit={handleSubmitTask} className={styles.formTasks}>
           <div className={styles.inputCreateTasks}>
-            <input placeholder='Adicione uma nova tarefa' />
+            <input onChange={handleInputCreateTask} value={task} placeholder='Adicione uma nova tarefa' required />
             <button type='submit'>
               <span>Criar</span>
               <PlusCircle size={20}/>
             </button>
           </div>
         </form>
-        <TaskStatusBar />
-        <div className={styles.tasksContainer}>       
-          { tasks.length > 0 ? <TaskList /> : <ZeroTasks /> }
+        <TaskStatusBar tasks={tasks} />
+        <div className={styles.tasksContainer}>   
+          {tasks.length > 0  
+            ? tasks.map((task) => {
+                return (
+                  <TaskList 
+                    key={task.id} 
+                    task={task}
+                    onToggleTask={onToggleTask}
+                    onDeleteTask={onDeleteTask}
+                  />
+                ) 
+              })           
+            : <ZeroTasks />   
+          }
         </div>
       </section>
     </div>
--- src/components/TaskStatusBar.tsx
+++ src/components/TaskStatusBar.tsx
@@ -1,15 +1,28 @@
 import styles from './TaskStatusBar.module.css'
 
-export function TaskStatusBar() {
+interface TaskProps {
+  id: string;
+  text: string;
+  checked: boolean;
+}
+
+interface TaskStatusBarProps {
+  tasks: TaskProps[];
+}
+
+export function TaskStatusBar({tasks}: TaskStatusBarProps) {
+  
+  const tasksDone = tasks.filter(task => task.checked === true);
+
   return (
     <div className={styles.taskStatusBar}>
       <div className={styles.statusTasks}>
         <span className={styles.createdText}>Tarefas criadas</span>
-        <span className={styles.badgeTasks}>5</span>
+        <span className={styles.badgeTasks}>{tasks.length}</span>
       </div>
       <div className={styles.statusTasks}>
         <span className={styles.doneText}>Concluídas</span>
-        <span className={styles.badgeTasks}>2 de 5</span>
+        <span className={styles.badgeTasks}>{tasksDone.length} de {tasks.length}</span>
       </div>
     </div>
   )
