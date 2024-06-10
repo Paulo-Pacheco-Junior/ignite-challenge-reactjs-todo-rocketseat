17d486c48ed3b568d8e52074822ec27143787ba5
Paulo-Pacheco-Junior <paulojrgyn88@gmail.com>
2024-06-10 19:33:35 -0300

    Persistência dos Dados das Tarefas no LocalStorage ao Criar, alternar status e deletar tarefas.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { ChangeEvent, FormEvent, useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
-import { Header } from './components/Header'
-import { TaskStatusBar } from './components/TaskStatusBar';
-import { ZeroTasks } from './components/ZeroTasks';
-import { TaskList } from './components/TaskList';
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { Header } from "./components/Header";
+import { TaskStatusBar } from "./components/TaskStatusBar";
+import { ZeroTasks } from "./components/ZeroTasks";
+import { TaskList } from "./components/TaskList";
 import { PlusCircle } from "@phosphor-icons/react";
-import styles from './App.module.css'
-import './global.css'
+import styles from "./App.module.css";
+import "./global.css";
 
 interface TaskProps {
   id: string;
@@ -16,7 +16,7 @@ interface TaskProps {
 
 export function App() {
   const [tasks, setTasks] = useState<TaskProps[]>([]);
-  const [task, setTask] = useState('');
+  const [task, setTask] = useState("");
 
   function handleInputCreateTask(event: ChangeEvent<HTMLInputElement>) {
     setTask(event.target.value);
@@ -24,29 +24,47 @@ export function App() {
 
   function handleSubmitTask(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    if(task.length > 0 && task.trim() !== '') {
+    if (task.length > 0 && task.trim() !== "") {
       const newTask = {
         id: uuidv4(),
-        text: task, 
-        checked: false
-      }
-      setTasks(tasks => [...tasks, newTask]);
-      setTask('');
+        text: task,
+        checked: false,
+      };
+
+      localStorage.setItem("@tasks", JSON.stringify([...tasks, newTask]));
+
+      setTasks((tasks) => [...tasks, newTask]);
+
+      setTask("");
     }
   }
 
   function onToggleTask(taskId: string) {
-    setTasks(tasks.map(task => {
-      if(task.id === taskId) {
-        return {...task, checked: !task.checked};
+    const toggledTask = tasks.map((task) => {
+      if (task.id === taskId) {
+        return { ...task, checked: !task.checked };
       }
       return task;
-    }));
+    });
+
+    localStorage.setItem("@tasks", JSON.stringify(toggledTask));
+
+    setTasks(toggledTask);
   }
 
-  function onDeleteTask(taskId: string) { 
-    setTasks(tasks.filter(task => task.id !== taskId));
+  function onDeleteTask(taskId: string) {
+    const filteredTasks = tasks.filter((task) => task.id !== taskId);
+
+    localStorage.setItem("@tasks", JSON.stringify(filteredTasks));
+
+    setTasks(filteredTasks);
   }
+
+  useEffect(() => {
+    const storedTasks = JSON.parse(localStorage.getItem("@tasks") || "[]");
+
+    setTasks(storedTasks);
+  }, []);
 
   return (
     <div className={styles.wrapper}>
@@ -54,30 +72,36 @@ export function App() {
       <section className={styles.mainContent}>
         <form onSubmit={handleSubmitTask} className={styles.formTasks}>
           <div className={styles.inputCreateTasks}>
-            <input onChange={handleInputCreateTask} value={task} placeholder='Adicione uma nova tarefa' required />
-            <button type='submit'>
+            <input
+              onChange={handleInputCreateTask}
+              value={task}
+              placeholder="Adicione uma nova tarefa"
+              required
+            />
+            <button type="submit">
               <span>Criar</span>
-              <PlusCircle size={20} className={styles.CreateButtonIcon}/>
+              <PlusCircle size={20} className={styles.CreateButtonIcon} />
             </button>
           </div>
         </form>
         <TaskStatusBar tasks={tasks} />
-        <div className={styles.tasksContainer}>   
-          {tasks.length > 0  
-            ? <ul className={styles.taskList}> 
-                {tasks.map((task) => (
-                  <TaskList 
-                    key={task.id} 
-                    task={task}
-                    onToggleTask={onToggleTask}
-                    onDeleteTask={onDeleteTask}
-                  />
-                ))}           
-              </ul>
-            : <ZeroTasks />   
-          }
+        <div className={styles.tasksContainer}>
+          {tasks.length > 0 ? (
+            <ul className={styles.taskList}>
+              {tasks.map((task) => (
+                <TaskList
+                  key={task.id}
+                  task={task}
+                  onToggleTask={onToggleTask}
+                  onDeleteTask={onDeleteTask}
+                />
+              ))}
+            </ul>
+          ) : (
+            <ZeroTasks />
+          )}
         </div>
       </section>
     </div>
-  )
+  );
 }
